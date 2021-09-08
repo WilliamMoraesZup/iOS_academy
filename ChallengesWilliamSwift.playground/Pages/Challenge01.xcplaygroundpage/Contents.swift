715c91d53fdfc02c1ed78720679164fa4eab4f4b
WilliamMoraesZup <william.moraes@zup.com.br>
2021-09-08 13:42:14 -0300

    Challenge 1 Done
    Challenge 1 - Done

---
+++ ChallengesWilliamSwift.playground/Pages/Challenge01.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+let validText = "abcdefg"
+let invalidText = "abfcgf"
+
+var checked = ""
+var repeteatedChar = ""
+
+func checkIfDontRepeat(text : String) -> Bool {
+    for char in text {
+        
+        if checked.contains(char) {
+            repeteatedChar.append(char)
+        }
+        checked.append(char)
+        print("Letras checadas = \(checked)")
+    }
+    
+    print("Letras repetidas: \(repeteatedChar.count)")
+    
+     return repeteatedChar.count>0 ?
+          false : true
+    
+}
+
+
+// Execuçao: Alterar pra trueText ou falseText
+
+checkIfDontRepeat(text: validText) ? print("A frase passou") : print("A frase não passou")
